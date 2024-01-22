0052f516a66ada472b9ec732719edc9f57c3eeb9
Nina Matsukura Bernardino <nmatsuk0@email.cpcc.edu>
2024-01-22 16:10:53 -0500

    Revert "HelloWorld Updated"
    This reverts commit ea3c7f62731769ff8bbc3d109ae4121e9d534f8a.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,3 @@ import java.util.Scanner;
 public class HelloWorld{
    public static void main(String[] args){
       System.out.println("Hello world!");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
-   }
-}   
